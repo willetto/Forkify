7499061959eb97385840201223449ebf1b6e737b
willetto <trey.willetto@gmail.com>
2022-04-20 13:33:20 -0600

    Fix fractional bug

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const SERVING_INCREMENT = 2;
-export const API_KEY = '33d9a6dd-5a27-47a3-8fbf-f592ce396432';
+export const API_KEY = '1a72ae9a-b0ee-42db-92e4-a4289ac9b027';
 export const MODEL_CLOSE_SEC = 1.5;
